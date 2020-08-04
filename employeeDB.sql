eb65ed01a571d10c62bd47ffd253674617f4efe2
Erin Fisher <flybirdie85@gmail.com>
2020-08-04 11:11:43 -0700

    added info to  employee database

--- employeeDB.sql
+++ employeeDB.sql
@@ -15,3 +15,29 @@ CREATE TABLE employee (
     PRIMARY KEY (id)
 
 );
+
+INSERT INTO employee (first_name, last_name, title, department,salary,manager)
+VALUES("John", "Doe", "Sales Lead", "Sales", 100000, "Ashley Rodriguez");
+
+INSERT INTO employee (first_name, last_name, title, department,salary,manager)
+VALUES("Mike", "Chan", "Salesperson", "Sales", 80000, "John Doe");
+
+INSERT INTO employee (first_name, last_name, title, department,salary,manager)
+VALUES("Ashley", "Rodriguez", "Lead Engineer", "Engineering", 150000, "null");
+
+INSERT INTO employee (first_name, last_name, title, department,salary,manager)
+VALUES("Kevin", "Tupik", "Software Engineer", "Engineering", 150000, "Ashley Rodriguez");
+
+INSERT INTO employee (first_name, last_name, title, department,salary,manager)
+VALUES("Malia", "Brown", "Accountant", "Finance", 125000, "null");
+
+INSERT INTO employee (first_name, last_name, title, department,salary,manager)
+VALUES("Sarah", "Lourd", "Legal Team Lead", "Legal", 250000, "null");
+
+INSERT INTO employee (first_name, last_name, title, department,salary,manager)
+VALUES("Tom", "Allen", "Lawyer", "Legal", 190000, "Sarah Lourd");
+
+INSERT INTO employee (first_name, last_name, title, department,salary,manager)
+VALUES("Tammer", "Galal", "Software Engineer", "Engineering", 120000, "Kevin Tupik");
+
+
