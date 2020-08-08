d0a25507458fd1a3139bd0894c716e589110d89b
Erin Fisher <flybirdie85@gmail.com>
2020-08-08 15:44:49 -0700

    needed to add foreign keys to tables in data base

--- employeeDB.sql
+++ employeeDB.sql
@@ -5,27 +5,28 @@ CREATE DATABASE employeesDB;
 USE employeesDB;
 
 CREATE TABLE employee (
-    id INT UNSIGNED AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER UNSIGNED NOT NULL,
-    manager_id INTEGER UNSIGNED,
-    PRIMARY KEY (id)
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER ,
+    FOREIGN KEY(role_id) REFERENCES roles(role_id) ON DELETE CASCADE,
+    FOREIGN KEY(manager_id) REFERENCES employee(employee_id) ON DELETE CASCADE
 
 );
 
 CREATE TABLE roles (
-    id INT UNSIGNED AUTO_INCREMENT,
-    title VARCHAR(30) UNIQUE NOT NULL,
+    role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,4) NOT NULL,
-    department_id INTEGER UNSIGNED NOT NULL,
-    PRIMARY KEY (id)
+    department_id INTEGER ,
+    FOREIGN KEY (department_id) REFERENCES departments(department_id) ON DELETE CASCADE
 
 ); 
 
 CREATE TABLE departments (
-    id INT UNSIGNED AUTO_INCREMENT,
-    names VARCHAR (30) UNIQUE NOT NULL,
+    department_id INT NOT NULL AUTO_INCREMENT,
+    department_names VARCHAR (30) NOT NULL,
     PRIMARY KEY(id)
 );
 
